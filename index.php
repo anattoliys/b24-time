e074a0e1362a09c23180a0bbcd0ca7c65af9e44e
Anatoliy <anattoliy90@gmail.com>
2020-03-12 00:19:16 +0300

    fix message new line

--- index.php
+++ index.php
@@ -16,6 +16,6 @@ $getDayTime = $dayTime->get();
 $monthTime = new MonthTime;
 $getMonthTime = $monthTime->get();
 
-$message = 'Время за день - ' . $getDayTime . '\n\nВремя за месяц - ' . $getMonthTime;
+$message = 'Время за день - ' . $getDayTime . "\n\nВремя за месяц - " . $getMonthTime;
 
 mail('anattoliy90@gmail.com', 'Time', $message);
