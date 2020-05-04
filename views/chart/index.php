746baf1bc442551370ddf1045dd9e02e01f93c3d
Anatoliy <anattoliy90@gmail.com>
2020-05-04 22:55:02 +0300

    added layouts

--- views/chart/index.php
+++ views/chart/index.php
@@ -1,3 +1,5 @@
-<?php
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/header.php';
 
 echo '<pre>';print_r('CHART');echo '</pre>';
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/footer.php';
