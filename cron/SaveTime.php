6ceb45bd29616de7cc78f87f62664e37b96cf0b3
Anatoliy <anattoliy90@gmail.com>
2020-05-06 22:56:00 +0300

    minor fix

--- cron/SaveTime.php
+++ cron/SaveTime.php
@@ -1,3 +1,8 @@
 <?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/Autoloader.php';
+
+Autoloader::register();
+
 Time::saveTime();
--- cron/SendTime.php
+++ cron/SendTime.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/Autoloader.php';
+
+Autoloader::register();
+
 $dayTime = new DayTime;
 $getDayTime = $dayTime->get();
 
