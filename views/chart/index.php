0ab0ef06b7bbf98c964407a74b9941accee00a26
Anatoliy <anattoliy90@gmail.com>
2020-06-24 21:42:25 +0300

    start chart

--- views/chart/index.php
+++ views/chart/index.php
@@ -1,5 +1,42 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/header.php'; ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/header.php';
 
-    <h1>Chart page</h1>
+$dbTime = Time::getTime();
 
-<? require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/footer.php';
+$hours = [];
+$dbDayTime = array_column($dbTime, 'dayTime');
+foreach ($dbDayTime as $item) {
+    $hours[] = date('h', strtotime($item));
+}
+$hours = implode(', ', $hours);
+
+$day = [];
+$dbDate = array_column($dbTime, 'date');
+foreach ($dbDate as $item) {
+    $day[] = date('d', strtotime($item));
+}
+$day = implode(', ', $day);
+?>
+    <link rel="stylesheet" href="/resources/css/Chart.min.css">
+
+    <canvas id="timeChart"></canvas>
+
+    <script src="/resources/js/Chart.min.js"></script>
+
+    <script>
+        var ctx = document.getElementById('timeChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [<?php echo $day; ?>],
+                datasets: [{
+                    label: 'Time is',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [<?php echo $hours; ?>]
+                }]
+            },
+            options: {}
+        });
+    </script>
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/footer.php';
