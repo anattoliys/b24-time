429a43c164b8e239c8f31eecc8be96cae338678f
Anatoliy <anattoliy90@gmail.com>
2020-09-29 21:02:01 +0300

    chart done

--- views/chart/index.php
+++ views/chart/index.php
@@ -1,58 +1,71 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/header.php';
 
-$dbTime = Time::getTime();
-
-$hours = [];
-$monthName = date('F');
-$dbDayTime = array_column($dbTime, 'dayTime');
-foreach ($dbDayTime as $item) {
-    $hours[] = date('h', strtotime($item));
-}
-$hours = implode(', ', $hours);
-
-$day = [];
-$dbDate = array_column($dbTime, 'date');
-foreach ($dbDate as $item) {
-    $day[] = date('d', strtotime($item));
-}
-$day = implode(', ', $day);
+$dateFormat = 'YYYY-MM-DD';
+$timeFormat = 'HH:mm:ss';
+$dbTime = Time::getCurrentMonthTime();
+$firstTimeItemDate = $dbTime[0]['date'];
+$currentMonthName = date('F', strtotime($firstTimeItemDate));
 ?>
+
+    <canvas id="currentMonthTimeChart"></canvas>
+
     <link rel="stylesheet" href="/resources/css/Chart.min.css">
-
-    <canvas id="timeChart"></canvas>
-
+    <script src="/resources/js/moment.min.js"></script>
     <script src="/resources/js/Chart.min.js"></script>
 
     <script>
-        var ctx = document.getElementById('timeChart').getContext('2d');
-        var chart = new Chart(ctx, {
+        let ctx = document.getElementById('currentMonthTimeChart').getContext('2d');
+        let chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: [<?php echo $day; ?>],
                 datasets: [{
+                    backgroundColor: 'rgba(59, 200, 245, .5)',
+                    borderColor: 'rgba(59, 200, 245, 1)',
                     label: 'Time is',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [<?php echo $hours; ?>]
-                }]
+                    
+                    data: [
+                        <?php foreach ($dbTime as $time): ?>
+                            {
+                                x: moment('<?php echo $time["date"] ?>', '<?php echo $dateFormat ?>'),
+                                y: moment('<?php echo $time["dayTime"] ?>', '<?php echo $timeFormat ?>'),
+                            },
+                        <?php endforeach; ?>
+                    ]
+                }]    
             },
             options: {
                 scales: {
-                    xAxes: [{
-                        display: true,
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Day of <?php echo $monthName; ?>'
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Day of <?php echo $currentMonthName ?>',
+						},
+                        type: 'time',
+                        time: {
+                            unit: 'day',
+                            displayFormats: {
+                                day: 'DD'
+                            },
+                            tooltipFormat: 'DD.MM.YYYY',
                         }
-                    }],
-                    yAxes: [{
-                        display: true,
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Hours'
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Hours',
+						},
+                        type: 'time',
+                        time: {
+                            unit: 'hour',
+                            displayFormats: {
+                                hour: 'HH:mm'
+                            },
+                            tooltipFormat: 'HH:mm',
                         },
-                    }]
-                }
+					}]
+				}
             }
         });
     </script>
