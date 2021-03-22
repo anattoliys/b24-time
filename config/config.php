b5b05b20a81390446c31c7f5e3204fd87abaf0ed
anattoliy90 <anattoliy90@gmail.com>
2021-03-22 20:29:20 +0300

    changed webhook

--- config/config.php
+++ config/config.php
@@ -5,3 +5,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'id12283674_dbtime');
 define('DB_USER', 'id12283674_admin');
 define('DB_PASS', 'h^=nVlmw[3<5<duE');
+define('B24_WEBHOOK', 'https://elize.bitrix24.ru/rest/1/wxy22hxxt8rtlpqm/');
