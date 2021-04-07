dc8dcbac3f5384b0cb0253315f34c18363cb28c0
anattoliy90 <anattoliy90@gmail.com>
2021-04-07 22:19:20 +0300

    minor fix

--- cron/SaveTime.php
+++ cron/SaveTime.php
@@ -8,14 +8,19 @@ $users = $userObj->getAll();
 
 if (!empty($users)) {
     foreach ($users as $user) {
-        $dayTime = new DayTime($user['b24Id']);
+        $dayTime = new DayTime($user);
         $getDayTime = $dayTime->get();
 
-        $monthTime = new MonthTime($user['b24Id']);
+        $monthTime = new MonthTime($user);
         $getMonthTime = $monthTime->get(true);
 
         $data = [
             'userId' => $user['id'],
+            'name' => $user['name'],
+            'chatId' => $user['chatId'],
+            'b24Id' => $user['b24Id'],
+            'rate' => $user['rate'],
+            'position' => $user['position'],
             'dayTime' => $getDayTime,
             'monthTime' => $getMonthTime,
         ];
--- test.php
+++ test.php
@@ -12,20 +12,26 @@ $users = $userObj->getAll();
 
 if (!empty($users)) {
     foreach ($users as $user) {
-        $dayTime = new DayTime($user);
-        $getDayTime = $dayTime->get();
+        if ($user['b24Id'] == 108) {
+            $dayTime = new DayTime($user);
+            $getDayTime = $dayTime->get();
 
-        $monthTime = new MonthTime($user['b24Id']);
-        $getMonthTime = $monthTime->get();
+            $monthTime = new MonthTime($user);
+            $getMonthTime = $monthTime->get();
 
-        $data = [
-            'name' => $user['name'],
-            'chatId' => $user['chatId'],
-            'rate' => $user['rate'],
-            'dayTime' => $getDayTime,
-            'monthTime' => $getMonthTime,
-        ];
+            $data = [
+                'userId' => $user['id'],
+                'name' => $user['name'],
+                'chatId' => $user['chatId'],
+                'b24Id' => $user['b24Id'],
+                'rate' => $user['rate'],
+                'position' => $user['position'],
+                'dayTime' => $getDayTime,
+                'monthTime' => $getMonthTime,
+            ];
 
-        // Log::getMessage($getDayTime);
+            // $telegramBot = new TelegramBot;
+            // $telegramBot->dispatcher($data, true);
+        }
     }
 }
