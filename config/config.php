294661aa4286487fa8414aaef581e309d98f1ab1
anattoliy90 <anattoliy90@gmail.com>
2021-04-12 20:57:02 +0300

    removed config

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'id12283674_dbtime');
-define('DB_USER', 'id12283674_admin');
-define('DB_PASS', 'h^=nVlmw[3<5<duE');
-define('B24_WEBHOOK', 'https://elize.bitrix24.ru/rest/1/i4lurt8444h1ggwu/');
